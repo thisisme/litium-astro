cd2476b244656faec36903c511d39a887b3b592b
Mats Olsberg <mats.olsberg@gmail.com>
2025-01-10 16:20:58 +0100

    Initial commit. Install packages and remove default components

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,12 @@
 // @ts-check
+import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [
+      tailwindcss()
+    ]
+  }
+});
